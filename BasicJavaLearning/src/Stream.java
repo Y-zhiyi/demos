7287762896382d9f7559d49958ac37183d63d77f
Y-zhiyi <18231113707@163.com>
2021-04-18 16:14:32 +0800

    *add gitignore

--- BasicJavaLearning/src/Stream.java
+++ BasicJavaLearning/src/Stream.java
@@ -4,5 +4,8 @@ public class Stream {
     public static void main(String[] args) {
         IntStream.range(1, 5).forEach(System.out::println);
         System.out.println(1213);
+
+        System.out.println(888888);
+
     }
 }
